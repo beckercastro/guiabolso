99911865c8d8ddf43f04e94719ee69343868f8ed
caue.becker <c.becker.de.castro@accenture.com>
2020-01-17 14:39:17 -0300

    create Dockerfiles

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -81,7 +81,15 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+# DATABASES = {
+#               'default': {
+#               'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#               'NAME': 'postgres',
+#               'USER': 'postgres',
+# #              'HOST': 'db',
+#               'PORT': 5432,
+#               }
+#             }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
