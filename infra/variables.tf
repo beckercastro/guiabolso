1b48db4c9344b9d0ffc3d8d5012bbdae48da0df5
caue.becker <c.becker.de.castro@accenture.com>
2020-01-20 14:53:24 -0300

    create a infra

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "bucket_region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  default = "cb-lambdas-dev"
+}
+
+variable "zip_file" {
+  default = "cb-arquivosgerenciaisg5.zip"
+}
